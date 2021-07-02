cd267216c2d252252111c826cb4260f7920b4b18
kellykang-tech <kellykang13@gmail.com>
2021-07-02 23:02:31 +0900

    :memo: Add: user-insert.

--- tutorial/blog/src/main/java/com/dev/blog/test/DummyControllerTest.java
+++ tutorial/blog/src/main/java/com/dev/blog/test/DummyControllerTest.java
@@ -8,10 +8,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.function.Supplier;
@@ -22,16 +20,37 @@ public class DummyControllerTest {
     @Autowired
     private UserRepository userRepository;
 
+    // email, password
+    // json 데이터를 요청 -> 스프링이 Java Object 로 변환해서 받아준다. (MessageConvert 의 Jackson 라이브러리가 변환해서 받아준다.) 이때 필요한 어노테이션이 @RequestBody
+    @Transactional
+    @PutMapping("/dummy/user/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+        System.out.println("id = " + id);
+        System.out.println("password = " + requestUser.getPassword());
+        System.out.println("email = " + requestUser.getEmail());
+
+        User user = userRepository.findById(id).orElseThrow(() -> {
+            return new IllegalArgumentException("수정에 실패하였습니다.");
+        });
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+
+//        userRepository.save(user);
+        return null;
+    }
+
     // http://localhost:8000/blog/dummy/user/
-    @GetMapping("/dummy/user")
+    @GetMapping("/dummy/users")
     public List<User> list() {
         return userRepository.findAll();
     }
 
     // 한 페이지당 2건의 데이터를 리턴받아 볼 예정
-    @GetMapping("/dummy/user/page")
-    public Page<User> pageList(@PageableDefault(size = 2, sort="id", direction = Sort.Direction.DESC)Pageable pageable) {
-        Page<User> users = userRepository.findAll(pageable);
+    @GetMapping("/dummy/user")
+    public List<User> pageList(@PageableDefault(size = 2, sort="id", direction = Sort.Direction.DESC)Pageable pageable) {
+        Page<User> pagingUser = userRepository.findAll(pageable);
+
+        List<User> users = pagingUser.getContent();
         return users;
     }
 
